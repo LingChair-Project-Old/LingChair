c2ea48136d64869358fd7c6860e6e625408c492c
MoonLeeeaf <150461955+MoonLeeeaf@users.noreply.github.com>
2024-07-17 00:49:50 +0800

    新增: npm run clear-http, 自动检测是否需要编译前端

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function fork(a) {
     let c = child_process.fork(a, {
         silent: true
     })
+    // 完美解决 Windows 下控制台日志乱码
     c.stdout.setEncoding('utf8')
     c.stdout.on('data', function (data) {
         console.log(data.toString().trim())
@@ -14,7 +15,13 @@ function fork(a) {
     return c
 }
 
-if (process.argv.includes("--build"))
+// 清理编译所得文件
+if (process.argv.includes("--clear-http")) {
+    io.remove(lingChairConfig.http.pathName)
+}
+
+// (编译文件并)运行服务
+if (process.argv.includes("--build") || !io.exists(lingChairConfig.http.pathName))
     fork('./src/builder.js').once('exit', (code) => {
         if (code != 0) {
             logger.error(`构建网页失败! 请查看日志寻找原因, 若多次无法解决请寻找他人解决`)
