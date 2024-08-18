c611f44075e4b538c0fb68dc63ce2cf99406e31c
MoonLeeeaf <150461955+MoonLeeeaf@users.noreply.github.com>
2024-08-18 22:29:31 +0800

    update

--- src/page/layout/ListItem.jsx
+++ src/page/layout/ListItem.jsx
@@ -8,18 +8,19 @@ import { showErrorImage } from '../utils.js'
  * @param { String } prop.image 图片链接
  * @param { String } prop.errorImage 错误时显示的图片链接
  * @param { String } prop.imageAlt 图片的文字提示
+ * @param { Object } prop.extra 额外数据
  * @returns { React.Component } component React组件
  */
 function ListItem(prop = {
     imageAlt: "未知图片",
+    extra: {},
 }) {
-    prop.image = prop.image ? prop.image : prop.errorImage
     return (
-        <mdui-list-item alignment="center" description={prop.content} {...prop} rounded style={{
+        <mdui-list-item alignment="center" description={prop.content} {...(prop.extra ? prop.extra : {})} rounded style={{
             width: '100%',
         }}>
             {prop.title}
-            <mdui-avatar slot="icon" src={prop.image} onError={() => showErrorImage(prop.errorImage, prop.imageAlt)}></mdui-avatar>
+            <mdui-avatar slot="icon" src={prop.image ? prop.image : prop.errorImage} onError={() => showErrorImage(prop.errorImage, prop.imageAlt)}></mdui-avatar>
         </mdui-list-item>
     )
 }
--- src/page/layout/MainPhone.jsx
+++ src/page/layout/MainPhone.jsx
@@ -4,42 +4,136 @@ import BottomBar from './BottomBar.js'
 import { handleRecentClick } from '../DrawerListApi.js'
 
 function MainPhone() {
-    let bottomNav = React.useRef(null)
+    function setTitle(t) {
+        $('#AppBarTitle').text(t)
+    }
+    function switchPage(c) {
+        let AppBarLeftButton = $("#AppBarLeftButton").get(0)
+        $('[page-bind]').hide()
+        $(`[page-bind=${c}]`).show()
+        if (c != 'chat') {
+            setTitle('铃之椅')
+            AppBarLeftButton.icon = 'menu'
+            AppBarLeftButton.onclick = () => {
+            
+            }
+        } else {
+            AppBarLeftButton.icon = 'back'
+            AppBarLeftButton.onclick = () => {
+                switchPage(BottomNav.value)
+            }
+        }
+    }
 
     React.useEffect(() => {
+        let BottomNav = $("#BottomNav").get(0)
         let e = (event) => {
-            alert(event.target.value)
+            let c = event.target.value
+            $('[bottom-nav-bind]').hide()
+            $(`[bottom-nav-bind=${c}]`).show()
         }
-        bottomNav.current.addEventListener('change', e)
+        BottomNav.addEventListener('change', e)
+        
+        e({target: BottomNav})
+        switchPage(BottomNav.value)
         
         // 如果不返回一个注销函数，会直接报错
         // 这个问题很困扰人
+        // 不 没有比使用非 React.createElement 更加烦人的事情
         return () => {
-            bottomNav.current.removeEventListener('change', e)
+            BottomNav.removeEventListener('change', e)
         }
-    }, [bottomNav])
+    })
     return (
         <div id="app-inner" style={{
             fontFamily: '-apple-system, system-ui, -webkit-system-font',
+            display: 'flex',
         }}>
             <mdui-top-app-bar variant="center-aligned">
-                <mdui-button-icon icon="menu" onClick={function(){
-                    
-                }}></mdui-button-icon>
-                <mdui-top-app-bar-title>铃之椅</mdui-top-app-bar-title>
+                <mdui-button-icon icon="back" id="AppBarLeftButton"></mdui-button-icon>
+                <mdui-top-app-bar-title id="AppBarTitle"></mdui-top-app-bar-title>
                 <div style={{
                     flexGrow: 1,
                 }}></div>
-                <mdui-button-icon icon="more_vert"></mdui-button-icon>
+                
+                <mdui-dropdown>
+                    <mdui-button-icon slot="trigger" icon="more_vert"></mdui-button-icon>
+                
+                    <mdui-menu>
+                        <mdui-menu-item bottom-nav-bind='recent'>清空非置顶</mdui-menu-item>
+                        <mdui-menu-item bottom-nav-bind='contacts'>添加新联系人</mdui-menu-item>
+                        <mdui-menu-item bottom-nav-bind='groups'>加入群聊</mdui-menu-item>
+                        <mdui-menu-item bottom-nav-bind='groups'>创建群聊</mdui-menu-item>
+                        <mdui-menu-item onClick={() => mdui.alert({
+                            headline: "关于",
+                            description: "铃之椅 v0.0.0☘️GitHub @LingChair",
+                            confirmText: "关闭",
+                        })}>关于</mdui-menu-item>
+                    </mdui-menu>
+                </mdui-dropdown>
             </mdui-top-app-bar>
                 
-            <BottomBar ref={bottomNav} value="recent" alignment="center" label-visibility="unlabeled">
-                <BottomBar.Item icon="watch_later--outlined" value="recent"></BottomBar.Item>
-                <BottomBar.Item icon="contacts" value="contects"></BottomBar.Item>
-                <BottomBar.Item icon="group" value="groups"></BottomBar.Item>
+            <BottomBar id="BottomNav" value="recent" alignment="center" label-visibility="selected">
+                <BottomBar.Item icon="watch_later--outlined" value="recent">最近</BottomBar.Item>
+                <BottomBar.Item icon="contacts" value="contacts">联系人</BottomBar.Item>
+                <BottomBar.Item icon="group" value="groups">群组</BottomBar.Item>
             </BottomBar>
             
-            <div id="Content"></div>
+            <div id="Content" style={{
+                display: 'flex',
+                width: '100%',
+            }}>
+                <div page-bind='recent' bottom-nav-bind='recent' style={{
+                    width: '100%',
+                }}>
+                    <mdui-list style={{
+                        width: '100%',
+                    }}>
+                        <ListItem
+                            title="测试"
+                            content="测试"
+                            targetId="Test"
+                            targetType="single"
+                            errorImage="res/default_avatar.png"
+                            imageAlt="测试的头像"
+                            extra={{
+                                onClick: handleRecentClick,
+                                style: {
+                                    width: '100%',
+                                },
+                            }} />
+                    </mdui-list>
+                </div>
+                <div page-bind='contacts' bottom-nav-bind='contacts' style={{
+                    width: '100%',
+                }}>
+                    <mdui-list style={{
+                        width: '100%',
+                    }}>
+                        <ListItem
+                            title="测试"
+                            targetId="Test"
+                            targetType="single"
+                            errorImage="res/default_avatar.png"
+                            imageAlt="测试的头像"
+                            extra={{
+                                onClick: (e) => {
+                                    switchPage('chat')
+                                    handleRecentClick(e)
+                                },
+                                style: {
+                                    width: '100%',
+                                },
+                            }} />
+                    </mdui-list>
+                </div>
+                <div page-bind='groups' bottom-nav-bind='groups'>
+                    群组
+                </div>
+                <div page-bind="chat" bottom-nav-bind='29495905696'>
+                    
+                </div>
+            </div>
         </div>
     )
 }
