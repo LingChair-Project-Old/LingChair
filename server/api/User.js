00470abccc5f973bbe86299bcb282bae6e6d6bd2
MoonLeeeaf <150461955+MoonLeeeaf@users.noreply.github.com>
2024-08-30 22:37:23 +0800

    update

--- server/api/User.js
+++ server/api/User.js
@@ -8,9 +8,45 @@
  * Organization - @LingChair <https://github.com/LingChair>
  */
 
-const io = require('../core/iolib')
+const io = require('../core/io')
 
-const vals = require("./val")
+const vals = require("../core/vals")
+
+let FriendTypeDef = {
+    /**
+     * 好友ID
+     * @type { String }
+     */
+    id,
+    /**
+     * 添加此好友的时间戳
+     * @type { Number }
+     */
+    addedTime,
+    /**
+     * 好友昵称
+     * @type { String }
+     */
+    nickName,
+}
+
+let GroupTypeDef = {
+    /**
+     * 群聊ID
+     * @type { String }
+     */
+    id,
+    /**
+     * 创建此群时的时间戳
+     * @type { Number }
+     */
+    createdTime,
+    /**
+     * 群名称
+     * @type { String }
+     */
+    name,
+}
 
 class User {
     /**
@@ -23,37 +59,104 @@ class User {
          * @type { String }
          */
         this.id = id
+        this.userConfigFile = User.openUserConfigFile(id)
+    }
+    /**
+     * 获取用户主配置文件
+     * @param { String } id 
+     * @returns { io }
+     */
+    static openUserConfigFile(id) {
+        return io.open(this.getDataPath() + "/user.json", "rw")
     }
     /**
      * 某个用户是否存在
      * @param { String } id 用户ID
+     * @returns { Boolean } 用户是否存在
      */
     static exists(id) {
-        io.exists(getUserPath(name))
+        return io.exists(this.getDataPath(id))
     }
     /**
      * 此用户是否存在
+     * @returns { Boolean } 用户是否存在
      */
     exists() {
         return User.exists(this.id)
     }
     /**
      * 获取某个账号使用的数据目录
+     * @param { String } id 用户ID
+     * @returns { String } 数据目录
      */
     static getDataPath(id) {
         return vals.LINGCHAIR_DATA_DIR + "/users/" + id
     }
     /**
      * 获取此账号使用的数据目录
+     * @returns { String } 数据目录
      */
     getDataPath() {
         return User.getDataPath(this.id)
     }
     /**
      * 获取此账号的密码
+     * @returns { String } 密码
      * @description 注意: 非明文, 是经过哈希运算的
      */
     getPassword() {
-        return io.open(getUserPath(name) + "/user.json").readJson().passwd
+        return this.userConfigFile.readAllJson().passwd
+    }
+    /**
+     * 设置头像
+     * @param { Buffer } bin 图片数据
+     */
+    setAvatarImage(bin) {
+        io.open(this.getDataPath() + "/avatar", "w").writeAll(head).close()
+    }
+    /**
+     * 设置用户昵称
+     * @param { String } nick 
+     */
+    setNickName(nick) {
+        let configIo = this.userConfigFile
+
+        let config = configIo.readAllJson()
+        config.nickName = nick
+        configIo.writeAllJson(config)
+    }
+    /**
+     * 获取某用户昵称
+     * @param { String } id 用户ID
+     * @returns { String } 用户昵称
+     */
+    static getNickName(id) {
+        return User.openUserConfigFile(id).readAllJsonAndClose().nickName
+    }
+    /**
+     * 获取用户昵称
+     * @returns { String } 用户昵称
+     */
+    getNickName() {
+        return this.userConfigFile.readAllJson().nickName
+    }
+    /**
+     * 
+     * @returns { contacts } 群聊和好友列表
+     */
+    getContacts() {
+        let contacts = {
+            /**
+             * 好友列表
+             * @type { FriendTypeDef[] }
+             */
+            friends: io.open(this.getDataPath() + '/friends.json', 'rw').readAllJsonAndClose(),
+            /**
+             * 群聊列表
+             * @type { GroupTypeDef[] }
+             */
+            groups: io.open(this.getDataPath() + '/groups.json', 'rw').readAllJsonAndClose()
+        }
+        return contacts
     }
 }
